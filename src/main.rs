52d089e50ea19ca72fb11c92a8fe17a71004ec70
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-01-22 13:21:25 -0400

    Added clap and built basic command structure
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,40 @@
-use std::env;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Task {
+        #[command(subcommand)]
+        command: Option<Tasks>,
+    },
+    Profile {
+        #[command(subcommand)]
+        command: Option<Profiles>,
+    },
+}
+
+#[derive(Subcommand)]
+enum Tasks {
+    Show {},
+    Add {},
+    Edit {},
+    Remove {},
+}
+
+#[derive(Subcommand)]
+enum Profiles {
+    Show {},
+    Add {},
+    Edit {},
+    Remove {},
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let _cli = Cli::parse();
 }
