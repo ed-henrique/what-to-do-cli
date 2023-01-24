9150a912787fe06a9985dbe9352cb1362bd77967
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-01-24 07:35:42 -0400

    Refactor consts
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- src/tasks.rs
+++ src/tasks.rs
@@ -4,28 +4,31 @@ use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::path::Path;
 
+// This code will be heavily changed when SQLite starts to be used, but for now it does what it's supposed to do
+
+static DIRECTORY_PATH: &str = "../tmp/";
+static FULL_PATH: &str = "../tmp/tmp_file.txt";
+
 pub fn add_task(task_message: &String) -> () {
-    // This code will be heavily changed when SQLite starts to be used, but for now it does what it's supposed to do
-
-    let file_name = "tmp_file.txt";
-    let directory_path = "../tmp/";
-    let full_path = &format!("{}{}", directory_path, file_name);
-
-    if !Path::new(directory_path).exists() {
-        fs::create_dir(directory_path).ok();
+    if !Path::new(DIRECTORY_PATH).exists() {
+        fs::create_dir(DIRECTORY_PATH).ok();
     }
 
-    if !Path::new(full_path).exists() {
-        File::create(full_path).ok();
+    if !Path::new(FULL_PATH).exists() {
+        File::create(FULL_PATH).ok();
     }
 
     let mut file = OpenOptions::new()
         .write(true)
         .append(true)
-        .open(full_path)
+        .open(FULL_PATH)
         .unwrap();
 
     if let Err(e) = writeln!(file, "{}", task_message) {
         eprintln!("Couldn't write to file: {}", e);
     }
+}
+
+pub fn show_tasks() -> () {
+
 }
