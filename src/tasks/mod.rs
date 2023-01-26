f9ac261b01be0f51cfbb2dde32998547afa65d80
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-01-26 07:17:41 -0400

    Change show_tasks to use sqlite instead of text file
    Signed-off-by: Eduardo Henrique <edu.hen.fm@gmail.com>

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -1,12 +1,11 @@
 pub mod models;
 pub mod schema;
 
-use what_to_do_cli::*;
 use diesel::prelude::*;
-use crate::models::NewTask;
+use models::{Task, NewTask};
 
 pub fn add_task(conn: &mut SqliteConnection, task_name: &str) -> usize {
-    use crate::schema::tasks;
+    use schema::tasks;
 
     let new_task = NewTask { task_name };
 
@@ -16,18 +15,15 @@ pub fn add_task(conn: &mut SqliteConnection, task_name: &str) -> usize {
         .expect("Error saving new post")
 }
 
-pub fn show_tasks() -> () {
+pub fn show_tasks(conn: &mut SqliteConnection) -> () {
     use self::schema::tasks::dsl::*;
 
-    let connection = &mut establish_connection();
     let results = tasks
-        .limit(5)
-        .load::<Task>(connection)
+        .load::<Task>(conn)
         .expect("Error loading tasks");
 
     println!("Displaying {} tasks", results.len());
     for task in results {
         println!("{}: {}", task.id, task.task_name);
-        println!("----------\n");
     }
 }
